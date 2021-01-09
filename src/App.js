7f74a15f00194e67bad4f8c85dd046c22d92aca3
alex9692 <battle253@gmail.com>
2021-01-09 12:38:12 +0530

    implemented routing functionality in the app and updated our menu-items with withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+
 import "./App.css";
 
 import Homepage from "./pages/Homepage/Homepage.component";
+const HatsPage = () => (
+  <div>
+    <h1>HATSPAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/" component={Homepage} />
+      </Switch>
     </div>
   );
 }
